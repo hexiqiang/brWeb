a8673cd085843c456d7686c73f6a696f5eab80aa
hexiqiang <376858010@qq.com>
2018-07-19 14:52:05 +0800

    自动测试测试文件发生变化

--- dj_tests.py
+++ dj_tests.py
@@ -1,9 +1,23 @@
 #coding=utf-8
 from selenium import webdriver
+import unittest
 import time
 #浏览器驱动
-driver=webdriver.Firefox()
-driver.get("http://127.0.0.1:8000")
-assert 'Django' in driver.title
-time.sleep(20)
-driver.close()
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        self.browser.get('http://127.0.0.1:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
